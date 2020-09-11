b81fbc4827c728cf32a64b376861acc6eb5fb68e
RYAN PILON <Ryanpilon1995@gmail.com>
2020-09-10 19:39:36 -0700

    added answers

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,12 +8,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a, b) { 
+    var a = 4 ;
+    var b = 7
+    return  a + b;
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+testSum(a, b);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +29,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    var a = 5;
+    var b = 9;
+    return product(a * b);
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(a,b);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,6 +53,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var a = [4, 7 , 5];
+    var b = [4,7,5];
+    var c = 
+
+    for 
 
 }
 
@@ -90,8 +100,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray(multArr) { 
+    var mult1 = multArr[0];
+    var mult2 = multArr[1];
+    var mult3 = multArr[2];
+    var prod1 = multiply(mult1 , mult2);
+    var prod2 = multiply(prod1 , mult3);
 
+    var string = 'The numbers, ' + multArr + 'have the product of ' + prod2 + '.';
+    return [prod2 , string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
